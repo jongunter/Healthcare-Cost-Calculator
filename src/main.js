a192dd19d447661a1b1c5abf7ec8da5ed3f05edf
Jon Gunter <jon@jongunter.com>
2017-06-26 19:59:04 -0600

    Fix charts so they are working

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const BILLED_AMOUNTS = [0, 100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 30000, 40000, 50000, 100000, 500000, 1000000];
 const CHART_CONTAINER = document.getElementById('chart');
 
+const BILLED_AMOUNTS_DOLLARS = BILLED_AMOUNTS.map(a => '$' + String(a));
 
 var data = [
   {
@@ -50,30 +51,44 @@ var hot = new Handsontable(container, {
   ]
 });
 
-function drawChart(data){
+function drawChart(plans){
+
+    var datasets = plans.map(planInfo => {
+            return {
+                label: planInfo.name,
+                data: planInfo.costs,
+                borderWidth: 1,
+                fill: false,
+                borderColor: randomColor()
+            }
+    });
+
+
     var myLineChart = new Chart(CHART_CONTAINER, {
         type: 'line',
-        data: data,
-        options: {
-
+        data: {
+            labels: BILLED_AMOUNTS_DOLLARS,
+            datasets: datasets
         }
-        
     });
 }
 
 
 function getCostChartData(plans){
 
-    return plans.map(function(plan){
-        var planInfo = {
-            name: plan.planName
-        }
-        var hcp = new HealthcarePlan(plan.monthlyPremium, plan.deductible, plan.coinsurance, plan.outOfPocketMax);
-        BILLED_AMOUNTS.forEach(function(amount){
-            planInfo['$' + amount] = hcp.caculateOutOfPocketCostForYear(amount);
-        })
+    return plans
+        .filter(assertValidPlan)
+        .map(function(plan){
+            var hcp = new HealthcarePlan(plan.monthlyPremium, plan.deductible, plan.coinsurance, plan.outOfPocketMax);
+            var planInfo = {
+                name: plan.planName,
+                costs: BILLED_AMOUNTS.map(amount => hcp.caculateOutOfPocketCostForYear(amount))
+            }
+            return planInfo;
+        });
 
-        return planInfo;
-    });
+}
 
+function assertValidPlan(plan){
+    return Boolean(plan.planName) && !isNaN(plan.monthlyPremium) && !isNaN(plan.deductible) && !isNaN(plan.coinsurance) && !isNaN(plan.outOfPocketMax)
 }
